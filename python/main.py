16a4a25034b049e80ca8607290fd3e16660800e6
Kevin Quinn <kpquinn2@wisc.edu>
2018-01-04 12:12:25 -0600

    Correct build_list funtion and add averages

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ from badsorts import common
 
 
 def build_list(length):
-    ints = list(range(1,length))
+    ints = list(range(1,length + 1))
     return common.shuffle(ints)
 
 
@@ -23,14 +23,26 @@ def main():
         print("Number of iterations must be positive.")
         raise SystemExit
 
+    if args.length < 1:
+        print("List can't be shorter than 1.")
+        raise SystemExit
+
+    total_time = 0
+    total_perms = 0
     print("$$$$$ Begin Bogosort $$$$$$$")
     for i in range(1, args.iterations + 1):
         random_list = build_list(args.length)
         duration, permutations = bogosort.run(random_list)
+        total_time += duration.total_seconds()
+        total_perms += permutations
         print("\n-----------------------")
         print("Run %d" % i)
         print("Time: %f s" % duration.total_seconds())
         print("Permutations: %d" % permutations)
+
+
+    print("\nAverage permutations: %f" % (total_perms / args.iterations))
+    print("Average time: %f" % (total_time / args.iterations))
 
 
 if __name__ == "__main__":
